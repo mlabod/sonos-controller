dadd5d4dbae37e66532b47d021f1d3575ace581a
Marvin <bugeyes.de@gmail.com>
2016-07-23 16:23:20 +0200

    new ip, shorter actions

--- actions/index.js
+++ actions/index.js
@@ -1,26 +1,14 @@
-export const play = () => {
-  return {
-    type: 'PLAY'
-  };
-};
+export const play = () => ({ type: 'PLAY' })
 
-export const pause = () => {
-  return {
-    type: 'PAUSE'
-  }
-}
+export const pause = () => ({ type: 'PAUSE' })
 
-export const playNext = () => {
-  return {
-    type: 'NEXT'
-  };
-};
+export const playNext = () => ({ type: 'NEXT' })
 
-export const playPrev = () => {
-  return {
-    type: 'PREV'
-  };
-};
+export const playPrev = () => ({ type: 'PREV' })
+
+export const switchRoom = (ip) => ({ type: 'SWITCH_ROOM', ip })
+
+export const roomInfo = (data) => ({ type: 'ROOM_INFO', data })
 
 export const songInfo = (data) => {
   return {
@@ -28,22 +16,5 @@ export const songInfo = (data) => {
     artist: data.artist,
     title: data.title,
     cover: data.cover
-  }
-}
-
-export const switchRoom = (ip) => {
-
-  console.log('SWITCH');
-
-  return {
-    type: 'SWITCH_ROOM',
-    ip
-  }
-}
-
-export const roomInfo = (data) => {
-  return {
-    type: 'ROOM_INFO',
-    data: data
   }
 }
